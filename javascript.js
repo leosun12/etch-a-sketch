b7f9c6e3382113ec4189604f5c2256bf1e4c23fe
Leo Sun <leohanchangsun@berkeley.edu>
2023-06-05 16:25:12 -0700

    add resize button

--- javascript.js
+++ javascript.js
@@ -12,3 +12,23 @@ for (let i = 0; i < 16 * 16; i++) {
     })
 }
 
+const sizer = document.querySelector('.sizer');
+sizer.addEventListener('click', generateGrid);
+
+function generateGrid() {
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    let size = +prompt('size');
+    for (let i = 0; i < size * size; i++) {
+        const grid = document.createElement('div');
+        grid.classList.add('grid');
+        grid.style.height = (1000 / size) - 2 + 'px'
+        grid.style.width = (1000 / size) - 2 + 'px'
+        container.appendChild(grid);
+        grid.addEventListener('mouseover', () => {
+            grid.classList.add('hovered');
+            console.log('hovering');
+        })
+    }
+}
